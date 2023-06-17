92c4ae3b17fd8f30e70dc98984a6aecdd9cfbcf3
mr21Awesomeness <78995257+brianlugo1@users.noreply.github.com>
2023-06-17 15:00:01 -0500

    Followed Yew simple tutorial!

---
+++ yew_tutorial_web_application/src/main.rs
@@ -0,0 +1,75 @@
+// Run `trunk serve --open` to start the development server
+// The port the server will run is 8080
+// The server can be found at `http://127.0.0.1:8080`
+
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+    html! {
+        <>
+            <div
+                class="
+                    w-100
+                    p-3
+                "
+                style="
+                    background-color: lightblue;
+                "
+            >
+                <h1>{ "Rust Web Application Yew!" }</h1>
+            </div>
+            <div
+                class="
+                    container
+                "
+                style="
+                    height: 93vh;
+                "
+            >
+                <div>
+                    <h1
+                        class="
+                            h3
+                        "
+                    >
+                        { "Introduction to Yew" }
+                    </h1>
+                    <p>
+                        { "The page you are view now was developed using Rust and a Web Framework called Yew!" }
+                    </p>
+                </div>
+                <div
+                    class="
+                        d-flex
+                        flex-column
+                        align-items-center
+                    "
+                >
+                    <p>{ *counter }</p>
+                    <button
+                        class="
+                            btn
+                            btn-primary
+                        "
+                        {onclick}
+                    >
+                        { "+" }
+                    </button>
+                </div>
+            </div>
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
